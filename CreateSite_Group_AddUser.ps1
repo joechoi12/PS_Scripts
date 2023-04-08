3a4ec68bd421b8e5b0bfd66756e73141cb82f794
Joe Choi <38042780+joechoi12@users.noreply.github.com>
2023-04-07 21:37:35 -0400

    Update CreateSite_Group_AddUser.ps1

--- CreateSite_Group_AddUser.ps1
+++ CreateSite_Group_AddUser.ps1
@@ -1,11 +1,11 @@
 #Provide your SharePoint Online Admin center URL
-$AdminSiteURL = "https://8n1q68-admin.sharepoint.com/"
+$AdminSiteURL = "https://something-admin.sharepoint.com/"
 #$AdminSiteURL = "https://<Tenant_Name>-admin.sharepoint.com"
   
 #Connect to SharePoint Tenant Admin Site
 try {
     Connect-PnPOnline -Url $AdminSiteURL -Interactive
-New-PnPSite -Type TeamSiteWithoutMicrosoft365Group -Title "New Site by PS Script 4" -Url "https://8n1q68.sharepoint.com/Sites/NewSiteFromPSScript4"
+New-PnPSite -Type TeamSiteWithoutMicrosoft365Group -Title "New Site by PS Script 4" -Url "https://someting.sharepoint.com/Sites/NewSiteFromPSScript"
 New-PnPGroup -Title "Custom"
 Get-PnPGroupMembers -Identity "Custom"
 Add-PnPUserToGroup -LoginName AdeleV@8n1q68.onmicrosoft.com -Identity "Custom"
